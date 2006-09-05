04cdd280ac01d6eedf50ac3ec19e2bbcceb3d0e3
chmaurer@iupui.edu <chmaurer@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-09-05 17:39:44 +0000

    initial import
    git-svn-id: https://source.sakaiproject.org/svn/metaobj/trunk@14225 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ api-impl/src/java/org/sakaiproject/metaobj/shared/mgt/HomeFactoryImpl.java
@@ -0,0 +1,125 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2004, 2005, 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.metaobj.shared.mgt;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.metaobj.shared.model.Id;
+import org.sakaiproject.metaobj.utils.xml.SchemaFactory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: John Ellis
+ * Date: Apr 9, 2004
+ * Time: 12:46:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HomeFactoryImpl implements HomeFactory {
+   protected final Log logger = LogFactory.getLog(getClass());
+   private List homeFactories = null;
+
+   public boolean handlesType(String objectType) {
+
+      for (Iterator i = homeFactories.iterator(); i.hasNext();) {
+         if (((HomeFactory) i.next()).handlesType(objectType)) {
+            return true;
+         }
+      }
+
+      return false;
+   }
+
+   public ReadableObjectHome getHome(String objectType) {
+
+      for (Iterator i = homeFactories.iterator(); i.hasNext();) {
+         HomeFactory testFactory = (HomeFactory) i.next();
+         if (testFactory.handlesType(objectType)) {
+            return testFactory.getHome(objectType);
+         }
+      }
+
+      return null;
+   }
+
+   public ReadableObjectHome findHomeByExternalId(String externalId, Id worksiteId) {
+
+      for (Iterator i = getHomeFactories().iterator(); i.hasNext();) {
+         ReadableObjectHome home = ((HomeFactory) i.next()).findHomeByExternalId(externalId, worksiteId);
+         if (home != null) {
+            return home;
+         }
+      }
+
+      return null;
+   }
+
+   public Map getHomes() {
+      Map homes = new Hashtable();
+
+      for (Iterator i = getHomeFactories().iterator(); i.hasNext();) {
+         homes.putAll(((HomeFactory) i.next()).getHomes());
+      }
+
+      return homes;
+   }
+
+   public Map getWorksiteHomes(Id worksiteId) {
+      Map homes = new Hashtable();
+
+      for (Iterator i = getHomeFactories().iterator(); i.hasNext();) {
+         homes.putAll(((HomeFactory) i.next()).getWorksiteHomes(worksiteId));
+      }
+
+      return homes;
+   }
+
+   public Map getHomes(Class requiredHomeType) {
+      Map homes = new Hashtable();
+
+      for (Iterator i = getHomeFactories().iterator(); i.hasNext();) {
+         homes.putAll(((HomeFactory) i.next()).getHomes(requiredHomeType));
+      }
+
+      return homes;
+   }
+
+   public void reload() {
+      SchemaFactory.getInstance().reload();
+      for (Iterator j = getHomes().values().iterator(); j.hasNext();) {
+         ReadableObjectHome home = (ReadableObjectHome) j.next();
+         home.refresh();
+      }
+   }
+
+   public List getHomeFactories() {
+      return homeFactories;
+   }
+
+   public void setHomeFactories(List homeFactories) {
+      this.homeFactories = homeFactories;
+   }
+}
---
+++ api-impl/src/java/org/sakaiproject/metaobj/shared/mgt/factories/DbXmlHomeFactoryImpl.java
@@ -0,0 +1,89 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2004, 2005, 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.metaobj.shared.mgt.factories;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.sakaiproject.metaobj.shared.mgt.HomeFactory;
+import org.sakaiproject.metaobj.shared.mgt.ReadableObjectHome;
+import org.sakaiproject.metaobj.shared.mgt.StructuredArtifactDefinitionManager;
+import org.sakaiproject.metaobj.shared.mgt.home.StructuredArtifactDefinition;
+import org.sakaiproject.metaobj.shared.model.Id;
+import org.sakaiproject.metaobj.shared.model.StructuredArtifactDefinitionBean;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: John Ellis
+ * Date: Apr 9, 2004
+ * Time: 12:51:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DbXmlHomeFactoryImpl extends HomeFactoryBase implements HomeFactory {
+
+   private StructuredArtifactDefinitionManager structuredArtifactDefinitionManager;
+
+   public boolean handlesType(String objectType) {
+      return (getHome(objectType) != null);
+   }
+
+   public Map getHomes(Class requiredHomeType) {
+      return super.getHomes(requiredHomeType);
+   }
+
+   public ReadableObjectHome findHomeByExternalId(String externalId, Id worksiteId) {
+      return createHome(getStructuredArtifactDefinitionManager().loadHomeByExternalType(externalId, worksiteId));
+   }
+
+   public ReadableObjectHome getHome(String objectType) {
+      return createHome(getStructuredArtifactDefinitionManager().loadHome(objectType));
+   }
+
+   public Map getWorksiteHomes(Id worksiteId) {
+      return createHomes(getStructuredArtifactDefinitionManager().getWorksiteHomes(worksiteId));
+   }
+
+   public Map getHomes() {
+      return createHomes(getStructuredArtifactDefinitionManager().getHomes());
+   }
+
+   protected Map createHomes(Map homeBeans) {
+      for (Iterator i = homeBeans.entrySet().iterator(); i.hasNext();) {
+         Map.Entry entry = (Map.Entry) i.next();
+         entry.setValue(createHome((StructuredArtifactDefinitionBean) entry.getValue()));
+      }
+      return homeBeans;
+   }
+
+   protected ReadableObjectHome createHome(StructuredArtifactDefinitionBean sadBean) {
+      return new StructuredArtifactDefinition(sadBean);
+   }
+
+   public StructuredArtifactDefinitionManager getStructuredArtifactDefinitionManager() {
+      return structuredArtifactDefinitionManager;
+   }
+
+   public void setStructuredArtifactDefinitionManager(StructuredArtifactDefinitionManager structuredArtifactDefinitionManager) {
+      this.structuredArtifactDefinitionManager = structuredArtifactDefinitionManager;
+   }
+
+}
