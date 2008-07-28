bbda3cddc9bb2467c1ce38776a6a23b653f6ba6a
david.horwitz@uct.ac.za <david.horwitz@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-07-28 14:56:02 +0000

    svn log -r45629 https://source.sakaiproject.org/svn/osp/trunk/ ------------------------------------------------------------------------ r45629 | john.ellis@rsmart.com | 2008-04-21 23:17:38 +0200 (Mon, 21 Apr 2008) | 2 lines
    SAK-12842
    
    ------------------------------------------------------------------------
    dhorwitz@david-horwitz-6:~/branchManagement/sakai_2-5-x> svn merge
    -c45629 https://source.sakaiproject.org/svn/osp/trunk/ osp
    U
    osp/matrix/api/src/java/org/theospi/portfolio/matrix/model/Cell.java
    U
    osp/matrix/tool/src/java/org/theospi/portfolio/matrix/control/CellFormPickerController.java
    U
    osp/matrix/tool/src/java/org/theospi/portfolio/matrix/control/ReviewHelperController.java
    U    osp/matrix/tool/src/webapp/WEB-INF/jsp/matrix/viewCell.jsp
    
    ------------------------------------------------------------------------
    dhorwitz@david-horwitz-6:~/branchManagement/sakai_2-5-x> svn merge
    -c-46549
    https://source.sakaiproject.org/svn/metaobj/branches/sakai_2-5-x/
    metaobj/
    U
    metaobj/metaobj-impl/api-impl/src/resources/org/sakaiproject/metaobj/shared/control/formCreate.xslt
    D
    metaobj/metaobj-api/api/src/java/org/sakaiproject/metaobj/shared/Helper.java
    U
    metaobj/metaobj-api/api/src/java/org/sakaiproject/metaobj/shared/model/ElementBean.java
    U
    metaobj/metaobj-util/tool-lib/src/java/org/sakaiproject/metaobj/shared/control/FormHelperController.java
    U
    metaobj/metaobj-util/tool-lib/src/java/org/sakaiproject/metaobj/shared/control/ToolFinishedView.java
    U
    metaobj/metaobj-util/tool-lib/src/java/org/sakaiproject/metaobj/shared/control/XsltArtifactView.java
    U
    metaobj/metaobj-util/tool-lib/src/java/org/sakaiproject/spring/util/SpringTool.java
    dhorwitz@david-horwitz-6:~/branchManagement/sakai_2-5-x> svn merge
    -c-46549 https://source.sakaiproject.org/svn/osp/branches/sakai_2-5-x/
    osp/
    U
    osp/matrix/api/src/java/org/theospi/portfolio/matrix/model/Cell.java
    U
    osp/matrix/tool/src/java/org/theospi/portfolio/matrix/control/CellFormPickerController.java
    U
    osp/matrix/tool/src/java/org/theospi/portfolio/matrix/control/ReviewHelperController.java
    U    osp/matrix/tool/src/webapp/WEB-INF/jsp/matrix/viewCell.jsp
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/metaobj/branches/sakai_2-5-x@49423 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- metaobj-util/tool-lib/src/java/org/sakaiproject/metaobj/shared/control/FormHelperController.java
+++ metaobj-util/tool-lib/src/java/org/sakaiproject/metaobj/shared/control/FormHelperController.java
@@ -22,13 +22,11 @@
 package org.sakaiproject.metaobj.shared.control;
 
 import org.sakaiproject.metaobj.utils.mvc.intf.Controller;
-import org.sakaiproject.metaobj.shared.Helper;
 import org.sakaiproject.content.api.ResourceEditingHelper;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.validation.Errors;
 
 import java.util.Map;
-import java.util.Hashtable;
 
 /**
  * Created by IntelliJ IDEA.
@@ -41,17 +39,11 @@ public class FormHelperController implements Controller {
 
    public ModelAndView handleRequest(Object requestModel, Map request, Map session,
                                      Map application, Errors errors) {
-      Map model = new Hashtable();
-      
-      if (request.get(Helper.HELPER_SESSION_ID) != null) {
-         model.put(Helper.HELPER_SESSION_ID, request.get(Helper.HELPER_SESSION_ID));
-      }
-      
       if (session.get(ResourceEditingHelper.ATTACHMENT_ID) != null) {
-         return new ModelAndView("edit", model);
+         return new ModelAndView("edit");
       }
       else {
-         return new ModelAndView("create", model);
+         return new ModelAndView("create");
       }
    }
 
