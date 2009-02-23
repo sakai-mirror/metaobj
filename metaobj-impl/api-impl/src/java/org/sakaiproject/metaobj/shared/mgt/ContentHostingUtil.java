3c9568d8e0fc85ce56a9b4980ed951398f161f1f
botimer@umich.edu <botimer@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-02-23 19:46:13 +0000

    SAK-15748 - Pulling owner and modifier information into artifact XML in trunk
    git-svn-id: https://source.sakaiproject.org/svn/metaobj/trunk@57919 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- metaobj-impl/api-impl/src/java/org/sakaiproject/metaobj/shared/mgt/ContentHostingUtil.java
+++ metaobj-impl/api-impl/src/java/org/sakaiproject/metaobj/shared/mgt/ContentHostingUtil.java
@@ -1,10 +1,16 @@
 package org.sakaiproject.metaobj.shared.mgt;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jdom.Element;
 import org.sakaiproject.content.api.ContentResource;
+import org.sakaiproject.component.cover.ComponentManager;
 import org.sakaiproject.time.api.Time;
 import org.sakaiproject.entity.api.EntityPropertyNotDefinedException;
 import org.sakaiproject.entity.api.EntityPropertyTypeException;
+import org.sakaiproject.user.api.User;
+import org.sakaiproject.user.api.UserDirectoryService;
+import org.sakaiproject.user.api.UserNotDefinedException;
 
 import java.util.Date;
 
@@ -16,6 +22,8 @@ import java.util.Date;
  * To change this template use File | Settings | File Templates.
  */
 public class ContentHostingUtil {
+
+	private static final Log log = LogFactory.getLog(ContentHostingUtil.class);
 
    public static Element createRepoNode(ContentResource contentResource) {
       Element repositoryNode;
@@ -34,8 +42,47 @@ public class ContentHostingUtil {
          repositoryNode.addContent(createNode("modified",
                modified.toString()));
       }
+      
+		UserDirectoryService uds = (UserDirectoryService) ComponentManager.get(UserDirectoryService.class);
+		String createdUser = contentResource.getProperties().getProperty(
+				contentResource.getProperties().getNamePropCreator());
+		if (createdUser != null) {
+			try {
+				User owner = uds.getUser(createdUser);
+				repositoryNode.addContent(createUserNode("owner", owner));
+			}
+			catch (UserNotDefinedException e) {
+				//we have something created by somebody we don't know -- this is worth logging
+				log.warn("Cannot find owner of content resource with reference: " + contentResource.getReference());
+			}
+		}
+      
+		String modifiedUser = contentResource.getProperties().getProperty(
+				contentResource.getProperties().getNamePropModifiedBy());
+		if (modifiedUser != null) {
+			try {
+				User modifiedBy = uds.getUser(modifiedUser);
+				repositoryNode.addContent(createUserNode("modifiedBy", modifiedBy));
+			}
+			catch (UserNotDefinedException e) {
+				//we have something modified by somebody we don't know -- this is worth logging
+				log.warn("Cannot find modifier of content resource with reference: " + contentResource.getReference());
+			}
+		}
+      
       return repositoryNode;
    }
+
+	private static Element createUserNode(String elementName, User user) {
+		Element userNode = new Element(elementName);
+		userNode.addContent(createNode("id", user.getId()));
+		userNode.addContent(createNode("eid", user.getEid()));
+		userNode.addContent(createNode("displayName", user.getDisplayName()));
+		userNode.addContent(createNode("email", user.getEmail()));
+		userNode.addContent(createNode("firstName", user.getFirstName()));
+		userNode.addContent(createNode("lastName", user.getLastName()));
+		return userNode;
+	}
 
    public static Element createNode(String name, String value) {
       Element newNode = new Element(name);
